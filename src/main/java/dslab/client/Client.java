c54dacbbdfd67a0b1a6faec239444399e3e67ecc
Ikurooo <ivanecankov@gmail.com>
2024-10-13 21:06:42 +0200

    client: added object variables

--- src/main/java/dslab/client/Client.java
+++ src/main/java/dslab/client/Client.java
@@ -8,29 +8,42 @@ import java.io.OutputStream;
 
 public class Client implements IClient {
 
+    private String componentId;
+    private ClientCLI cli;
+    private InputStream in;
+    private OutputStream out;
+
     public Client(String componentId, InputStream in, OutputStream out) {
+        this.componentId = componentId;
+        this.in = in;
+        this.out = out;
     }
 
     @Override
     public void run() {
-        throw new RuntimeException("Not implemented yet.");
+        // TODO: add real config
+        this.cli = new ClientCLI(this, null, this.in, this.out);
+        this.cli.run();
     }
 
     @Override
     public String getComponentId() {
-        return "";
+        return this.componentId;
     }
 
     @Override
     public void shutdown() {
-        throw new RuntimeException("Not implemented yet.");
+        this.cli.shutdown();
+
     }
 
     /**
      * Creates a new {@link Client} and runs it.
-     * Standard input, output and error streams are passed to the client which are then used by the {@link ClientCLI}.
+     * Standard input, output and error streams are passed to the client which are
+     * then used by the {@link ClientCLI}.
      *
-     * @param args the client config filename found in classpath resources without the file extension
+     * @param args the client config filename found in classpath resources without
+     *             the file extension
      */
     public static void main(String[] args) {
         ComponentFactory.createClient(args[0], System.in, System.out).run();
